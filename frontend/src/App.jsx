00b6ee46543a9af04790028c2629c1676b8e1a74
ADITHYA-ATIKUKKE <adithyaprovab@gmail.com>
2026-09-22 10:46:02 +0530

    Day 2: Add and delete tasks from React UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,7 @@ function Taskinput(){
 function App() {
   const [tasks,setTasks] = useState([]);
   const [loading,setLoding] = useState(true);
+  const [newTitle,setNewTitle] = useState("");
 
   useEffect(()=>{
     fetch('http://localhost:3001/tasks').then(res=>res.json()).then(data=>{
@@ -41,13 +42,49 @@ function App() {
     })
     .catch(err=>console.error('Error fetching tasks:', err))
   },[]);
+  const handleAddTask = () => {
+    if(!newTitle.trim()) return;
+    fetch('http://localhost:3001/tasks',{
+      method:'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body:JSON.stringify({title:newTitle})
+    })
+    .then(res=>res.json())
+    .then(createdTask=>{
+      setTasks([...tasks,createdTask]);
+      setNewTitle('');
+    })
+    .catch(err => console.error('Error adding task:', err));
+  }
+  const handleTaskDelete = (id) => {
+    fetch(`http://localhost:3001/tasks/${id}`,{
+      method:'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res=>res.json())
+    .then(deletedTask=>{
+      setTasks(tasks.filter(task=>task.id!==deletedTask.id));
+      
+    })
+    .catch(err => console.error('Error adding task:', err));
+  }
   if(loading) return <p>Loading...</p>;
   return (
    <div>
     <h1>My Tasks</h1>
+    <input
+        type="text"
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+        placeholder="New task title"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
     <ul>
       {tasks.map(task=>(
-        <li key={task.id}>{task.title} - {task.done? 'Done' : 'Pending'}</li>
+        <li key={task.id}>
+          {task.title} - {task.done? 'Done' : 'Pending'}
+          <button onClick={()=>handleTaskDelete(task.id)}>Delete</button>
+          </li>
       ))}
     </ul>
    </div>
